deb47bec37f7e9fa650b0877db8ba6379f42faf0
George <georgeoluwasemilore1@gmail.com>
2023-09-04 14:24:25 +0100

    test

---
+++ javascript-warm_up/103-object_fct.js
@@ -0,0 +1,18 @@
+const myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+  /*
+  YOUR CODE HERE
+  */
+function incr() {
+  this.value++;
+}
+myObject.incr = incr;
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
