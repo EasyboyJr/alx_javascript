e8117945201627b6a606ce45d52d1b9654b4eb52
George <georgeoluwasemilore1@gmail.com>
2023-09-06 13:53:00 +0100

    fixed an error

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -7,8 +7,9 @@ class Rectangle {
     }
 
     rotate() {
+        const temp = this.width
         this.width = this.height;
-        this.height = this.width;
+        this.height = temp;
     }
 
     double() {
