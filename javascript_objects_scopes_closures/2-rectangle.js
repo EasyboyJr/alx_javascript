144760f6d5cb79176cf45400ea11e646052ccd06
George <georgeoluwasemilore1@gmail.com>
2023-09-06 12:22:55 +0100

    removed uneccessary blocks

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,10 +4,6 @@ class Rectangle {
         if (w > 0 && h > 0) {
             this.width = w;
             this.height = h;
-        } else {
-            const emptyObject = Object.create(null);
-            emptyObject.constructor = Rectangle;
-            return emptyObject;
         }
     }
 }
